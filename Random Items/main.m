289c5a1b9b2698dae9968e0d042dc312427a6f2e
Ryan D'souza <dsouzarc@gmail.com>
2015-01-20 23:48:48 -0500

    Initial Commit

---
+++ Random Items/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Random Items
+//
+//  Created by Ryan D'souza on 1/20/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
